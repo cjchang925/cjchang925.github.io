9178ce3c570106cbb6b877bac9f92cebaa955b45
Chi-Chun Chang <cjchang925@gmail.com>
2024-04-25 14:05:20 +0800

    調整動畫邏輯，執行完上一個動畫自動觸發下一個動畫

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import {
   trigger,
-  state,
   style,
   animate,
   transition,
   stagger,
   query,
 } from '@angular/animations';
+import { greetings } from './home';
 
 @Component({
   selector: 'app-home',
@@ -16,10 +16,14 @@ import {
   animations: [
     trigger('appearAnimation', [
       transition('* => *', [
-        query(':enter', [
-          style({ opacity: 0 }),
-          stagger(50, [animate('50ms', style({ opacity: 1 }))]),
-        ], { optional: true }),
+        query(
+          ':enter',
+          [
+            style({ opacity: 0 }),
+            stagger(50, [animate('50ms', style({ opacity: 1 }))]),
+          ],
+          { optional: true }
+        ),
       ]),
     ]),
   ],
@@ -28,32 +32,17 @@ export class HomeComponent {
   /**
    * 首頁介紹文字
    */
-  public greetings: string[][] = [
-    ['👋', ' ', 'H', 'i', ',', ' ', 'I', ' ', 'a', 'm'],
-    ['🙂', ' ', 'C', 'h', 'i', '-', 'C', 'h', 'u', 'n', ' ', 'C', 'h', 'a', 'n', 'g', ','],
-    ['👨‍💻', ' ', 'A', ' ', 'F', 'u', 'l', 'l', ' ', 'S', 't', 'a', 'c', 'k', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', ','],
-    ['😉', ' ', 'W', 'e', 'l', 'c', 'o', 'm', 'e', ' ', 't', 'o', ' ', 'm', 'y', ' ', 'L', 'a', 'b', '!'],
-  ]
+  public greetings: string[][] = greetings;
 
   /**
    * 首頁介紹文字動畫狀態
    */
   public animationState: number = 0;
 
-  ngOnInit() {
-    // 等待 1 秒後，開始播放第二行文字
-    setTimeout(() => {
-      this.animationState = 1;
-    }, 500);
-
-    // 等待 1.9 秒後，開始播放第三行文字
-    setTimeout(() => {
-      this.animationState = 2;
-    }, 1400);
-
-    // 等待 3.2 秒後，開始播放第四行文字
-    setTimeout(() => {
-      this.animationState = 3;
-    }, 2700);
+  /**
+   * 每一行的動畫完成後，將狀態加一，觸發下一行的動畫
+   */
+  public animationDone(): void {
+    this.animationState++;
   }
 }
